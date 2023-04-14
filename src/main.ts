e5bb3327bf6fb6d4a68ae4c78fa849b1107817bd
brunof <dlqq@outlook.es>
2023-04-14 20:14:06 -0300

    Commit Despliegue Front

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,27 @@ async function bootstrap() {
 
   //***************************CORS***************************************/
 
+  /*
   app.enableCors({
     origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   });
+  */
 
+  app.enableCors({
+    origin: ['https://desplieguefront.web.app', 'http://localhost:4200'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://desplieguefront.web.app, http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
   //**********************************************************************/
   
   await app.listen(process.env.PORT);
